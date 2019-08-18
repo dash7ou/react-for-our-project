e369c02320504a0dff8d102f7a460f2e7aa3bc13
mohammed zourob <fxfddfno@gmail.com>
2019-08-18 23:47:54 +0300

    add socktio clint and listen to delete and update and create post

---
+++ README.md
@@ -0,0 +1,8 @@
+# react-for-our-project
+Restful API
+
+
+this is react for [project nodejs post maneger](https://github.com/dash7ou/post-manger-nodejs)
+
+the [orignal project](https://mega.nz/#!ufASXSoB!ejowPjm_T-C_l-FMcigJLlI2gKxTjBGmHvqS6boG5Zo) after make some adding download mega
+
--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -8,6 +8,7 @@ import Paginator from "../../components/Paginator/Paginator";
 import Loader from "../../components/Loader/Loader";
 import ErrorHandler from "../../components/ErrorHandler/ErrorHandler";
 import "./Feed.css";
+import openSocket from "socket.io-client";
 
 class Feed extends Component {
   state = {
@@ -35,8 +36,40 @@ class Feed extends Component {
       .catch(this.catchError);
 
     this.loadPosts();
+    const socket = openSocket("http://localhost:8080");
+    socket.on("posts", data => {
+      if (data.action === "created") {
+        this.addPost(data.post);
+      } else if (data.action === "updated") {
+        this.updatePost(data.post);
+      } else if (data.action == "deleted") {
+        this.loadPosts();
+      }
+    });
   }
 
+  addPost = post => {
+    this.setState(prevState => {
+      const updatedPosts = [...prevState.posts];
+      if (prevState.postPage === 1) {
+        updatedPosts.pop();
+        updatedPosts.unshift(post);
+      }
+      return { posts: updatedPosts, totalPosts: prevState.totalPosts + 1 };
+    });
+  };
+  updatePost = post => {
+    this.setState(prevState => {
+      const updatedPosts = [...prevState.posts];
+      const updatedPostIndex = updatedPosts.findIndex(p => p._id === post._id);
+      if (updatedPostIndex > -1) {
+        updatedPosts[updatedPostIndex] = post;
+      }
+      return {
+        posts: updatedPosts
+      };
+    });
+  };
   loadPosts = direction => {
     if (direction) {
       this.setState({ postsLoading: true, posts: [] });
@@ -148,17 +181,7 @@ class Feed extends Component {
           createdAt: resData.post.createdAt
         };
         this.setState(prevState => {
-          let updatedPosts = [...prevState.posts];
-          if (prevState.editPost) {
-            const postIndex = prevState.posts.findIndex(
-              p => p._id === prevState.editPost._id
-            );
-            updatedPosts[postIndex] = post;
-          } else if (prevState.posts.length < 2) {
-            updatedPosts = prevState.posts.concat(post);
-          }
           return {
-            posts: updatedPosts,
             isEditing: false,
             editPost: null,
             editLoading: false
@@ -196,10 +219,11 @@ class Feed extends Component {
       })
       .then(resData => {
         console.log(resData);
-        this.setState(prevState => {
-          const updatedPosts = prevState.posts.filter(p => p._id !== postId);
-          return { posts: updatedPosts, postsLoading: false };
-        });
+        this.loadPosts();
+        // this.setState(prevState => {
+        //   const updatedPosts = prevState.posts.filter(p => p._id !== postId);
+        //   return { posts: updatedPosts, postsLoading: false };
+        // });
       })
       .catch(err => {
         console.log(err);
